2ab74893a763df1e083d6978f41470b33dcfb2ff
twdockery <62146335+twdockery@users.noreply.github.com>
2020-04-25 14:46:43 -0400

    I am revisiting earlier projects and integrating new knowledge into earlier projects. Old versions should still be available through github versioning

--- Madlibs.py
+++ Madlibs.py
@@ -4,7 +4,7 @@ familyFriend = input("Who is a family friend? ")
 food = input("What is your favorite food? ")
 action = input("What's your favorite marial arts move? ")
 body = input("What's their best body part? ")
-print
-print('%s leaned in and said: "We weren\'t expecting company tonight. I\'m sorry but there won\'t be enough %s for '
-      'everyone. We\'ll be giving you a smaller portion than usual, and there won\'t be enough for seconds. That\'s '
-      'when, I decided to %s them in the %s!' % (familyFriend, food, action, body))
+print()
+print('%s leaned in and said: "We weren\'t expecting this much company tonight. I\'m sorry but there won\'t be enough '
+      '%s for everyone. We\'ll be giving you a smaller portion than usual, and there won\'t be enough for seconds. '
+      'In retaliation, I decided to %s %s in the %s!' % (familyFriend, food, action, familyFriend, body))
