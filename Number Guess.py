eecb36f8eca35cdc77399848b6f11bbd1f8a384b
twdockery <62146335+twdockery@users.noreply.github.com>
2020-04-26 17:45:00 -0400

    Trying to deal with functions effectively

--- Number Guess.py
+++ Number Guess.py
@@ -1,26 +1,23 @@
 from random import randrange #this allows us to create random numbers
-secret = (randrange(10) + 1 # this is a random number from 0-9 + 1
-def guess_it():
-    """
-    User will guess until they have the secret number
-    """
-    guess = int(input("Guess a number from 1 to 10: "))
-    print(secret)
-    while guess != secret:
-        if guess < secret:
-            print("Too small, Try again!")
-            guess_it()
-        elif guess > secret:
-            print("Too big, Try again!")
-            guess_it()
-        else:
-            print("Congratulations!")
-            print("Congratulations!")
-            print("Congratulations!")
-            print("Congratulations!")
-            print("Congratulations!")
-            print("Congratulations!")
-            print("Congratulations!")
-            print("Congratulations!")
+secret = (int(randrange(10)) + 1)
+def guessing():
+    print("Pick a number from 1 to 10: ")
+    guess = int(input("Guess which number it is: "))
+    if guess > secret:
+        print("Too big...")
+        guessing()
+    elif guess < secret:
+        print("Too small...")
+        guessing()
+    else:
+        print("Congratulations!")
+        print("Congratulations!")
+        print("Congratulations!")
+        print("Congratulations!")
+        print("Congratulations!")
+        print("Congratulations!")
+        print("Congratulations!")
+        print("Congratulations!")
+        print("Your guess of %s was correct!" % (guess))
 
-guess_it()
+guessing()
