7aeef48ecb2866b36a6b1109433497850e1eaffe
twdockery <62146335+twdockery@users.noreply.github.com>
2020-04-28 22:08:05 -0400

    Updating list and dictionaries

---
+++ Shopping-LIst.py
@@ -0,0 +1,25 @@
+﻿shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
+    
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+
+def compute_bill(food):
+  total = 0
+  for everyItem in food:
+    if stock[everyItem] > 0:
+        total += prices[everyItem]
+        stock[everyItem] -= 1
+  return total
+
+compute_bill(shopping_list)
