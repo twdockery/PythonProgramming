2bfb8a782aa029b2bfe5f3adec28b9599bbd2971
twdockery <62146335+twdockery@users.noreply.github.com>
2020-05-02 19:58:29 -0400

    Updating list and dictionaries

--- Grade Summaries.py
+++ Grade Summaries.py
@@ -1,22 +1,23 @@
-#In this program, grades have been added to dictionaries
+# In this program, grades have been added to dictionaries
 lloyd = {
-  "name": "Lloyd",
-  "homework": [90.0, 97.0, 75.0, 92.0],
-  "quizzes": [88.0, 40.0, 94.0],
-  "tests": [75.0, 90.0]
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
 }
 alice = {
-  "name": "Alice",
-  "homework": [100.0, 92.0, 98.0, 100.0],
-  "quizzes": [82.0, 83.0, 91.0],
-  "tests": [89.0, 97.0]
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
 }
 tyler = {
-  "name": "Tyler",
-  "homework": [0.0, 87.0, 75.0, 22.0],
-  "quizzes": [0.0, 75.0, 78.0],
-  "tests": [100.0, 100.0]
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
 }
+
 
 def average(numbers):
     total = sum(numbers)
@@ -32,13 +33,29 @@ def get_average(student):
     total = homework * .1 + quizzes * .3 + tests * .6
     return total
 
+
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
+
 # a list is built including all the students names
 students = (lloyd, alice, tyler)
 
 for everyItem in students:
-  print(everyItem["name"])
-  print("Homework Grades: %s" % (everyItem["homework"]))
-  print("Quiz Grades: %s" % (everyItem["quizzes"]))
-  print("Test Grades: %s" % (everyItem["tests"]))
-  print("Final Grade at this time: %s" % (round(get_average(everyItem),2))) # rounds the variable to 2 places
-  print("")
+    print(everyItem["name"])
+    print("Homework Grades: %s" % (everyItem["homework"]))
+    print("Quiz Grades: %s" % (everyItem["quizzes"]))
+    print("Test Grades: %s" % (everyItem["tests"]))
+    print("Final Grade at this time: %s (%s)" % (
+    get_letter_grade(get_average(everyItem)), round(get_average(everyItem), 2)))  # rounds the variable to 2 places
+    print("")
+
