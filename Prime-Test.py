e14493ba84003ca4e1c74557f23abf0ae2110ba1
twdockery <62146335+twdockery@users.noreply.github.com>
2020-05-13 22:19:41 -0400

    Created Prime Number Checking program

--- Prime-Test.py
+++ Prime-Test.py
@@ -1,12 +1,13 @@
 def is_prime(num):
     factor = num
-    for i in range(1,num):
+    for i in range(2,num):
         num -= 1
         if factor % num == 0:
             print("This number is NOT prime. %s is a factor of %s" % (num, factor))
             break
-        else:
-            if num == 1:
-                print("This number is a PRIME")
 
+        if num == 2:
+            print("This number is a PRIME")
+
+print("Prime Number Checker!")
 is_prime(int(input("what is your number: ")))
