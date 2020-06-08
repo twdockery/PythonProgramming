f0d4b3ff3d5d757ea65e285181ca5b2cca32dd4f
twdockery <62146335+twdockery@users.noreply.github.com>
2020-06-08 12:47:39 -0400

    First Major Project (main.py and student.py) including 2 custom modules and 5 functions.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 #Tyler Dockery for CSC121  6-8-2020  twdockery@waketech.edu
+# ----------------------------------------------------------------
+# Author: Tyler Dockery
+# Date: Completed 6-8-2020
+#
+# This program creates a class registration system.  It allows
+# students to add courses, drop courses and list courses they are
+# registered for.
+# -----------------------------------------------------------------
+
 import student
+
+
 def login(id, s_list):
+
+    # ------------------------------------------------------------
+    # This function allows a student to log in.
+    # It has two parameters: id and s_list, which is the student
+    # list. This function asks user to enter PIN. If the ID and PIN
+    # combination is in s_list, display message of verification and
+    # return True. Otherwise, display error message and return False.
+    # -------------------------------------------------------------
+
     # print("login called") #debug area
     pin = input("Enter PIN: ")
     if (id,pin) in s_list:
@@ -12,6 +32,17 @@ def login(id, s_list):
 
 
 def main():
+
+    # ------------------------------------------------------------
+    # This function manages the whole registration system.  It has
+    # no parameter.  It creates student list, course list,
+    # max class size list and roster list.  It uses a loop to serve
+    # multiple students. Inside the loop, ask student to enter ID,
+    # and call the login function to verify student's identity. Then
+    # let student choose to add course, drop course or list courses.
+    # This function has no return value.
+    # -------------------------------------------------------------
+
     student_list = [('1001', '111'), ('1002', '222'), ('1003', '333'), ('1004', '444')]
     course_list = ['CSC101','CSC102','CSC103']
     max_size_list = [3, 2, 1]
--- student.py
+++ student.py
@@ -1,6 +1,21 @@
 #tyler dockery for CSC121  6-8-2020
 
 def add_course(id, c_list, m_list, r_list):
+
+    # ------------------------------------------------------------
+    # This function adds a student to a course.  It has four
+    # parameters: id is the ID of the student to be added; c_list
+    # is the course list; r_list is the list of class rosters;
+    # m_list is the list of maximum class sizes.  This function
+    # asks user to enter the course he/she wants to add.  If the
+    # course is not offered, display error message and stop.
+    # If the course is full, display error message and stop.
+    # If student has already registered for this course, display
+    # error message and stop.  Add student ID to the course’s
+    # roster and display a message if there is no problem.  This
+    # function has no return value.
+    # -------------------------------------------------------------
+
     add = input("Add which course: ")
     if add == "0":
         choice = "0"
@@ -29,6 +44,19 @@ def add_course(id, c_list, m_list, r_list):
 
 
 def drop_course(id, c_list, r_list):
+
+    # ------------------------------------------------------------
+    # This function drops a student from a course.  It has three
+    # parameters: id is the ID of the student to be dropped;
+    # c_list is the course list; r_list is the list of class
+    # rosters. This function asks user to enter the course he/she
+    # wants to drop.  If the course is not offered, display error
+    # message and stop.  If the student is not enrolled in that
+    # course, display error message and stop. Remove student ID
+    # from the course’s roster and display a message if there is
+    # no problem.  This function has no return value.
+    # -------------------------------------------------------------
+
     drop = input("drop which course: ")
     # print(add) # debug area
     if drop.upper() not in c_list:
@@ -44,6 +72,14 @@ def drop_course(id, c_list, r_list):
 
 
 def list_courses(id, c_list, r_list):
+
+    # ------------------------------------------------------------
+    # This function displays and counts courses a student has
+    # registered for.  It has three parameters: id is the ID of the
+    # student; c_list is the course list; r_list is the list of
+    # class rosters. This function has no return value.
+    # -------------------------------------------------------------
+
     print("Courses registered:")
     classes = []
     size = 0
