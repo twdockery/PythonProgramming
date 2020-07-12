0246137305bba826b123c3cceec54bce6ea836fb
twdockery <62146335+twdockery@users.noreply.github.com>
2020-07-12 12:26:04 -0400

    Post Midterm, Dealing with sets and dictionaries

--- test.py
+++ test.py
@@ -1,17 +1,28 @@
-# Tyler Dockery for CSC121 5-18-2020 twdockery@waketech.edu
-"""
-A hotdog stand sells hotdogs, potato chips and sodas.  Hotdogs are $2.50 each.  Potato chips are $1.50 per bag.  Sodas are $1.25 per cans.  Design a program to do the following.  Ask the user to enter number of hotdogs, chips and sodas ordered by the customer.  The program will calculate and display the total amount due.
-Step 1: input the number of hotdogs
-Step 2: input the number of potato chip bags
-Step 3: input the number of soda cans
-Step 4: calculate cost = (hotdogs * 2.5) + (potato chip bags * 1.5) + (soda cans * 1.25)
-Step 5: display cost
-"""
+list1 = []
+dictionary1 = {}
+master_string = input("enter a string: ")
 
-hotdogs = int(input("How many hot dogs did the customer order? "))
-if not type(hotdogs) is int:
-    raise TypeError("Only integers are allowed")
-chips = int(input("How many bags of potato chips doid the customer order? "))
-soda = int(input("How many cans of soda did the customer order? "))
-total = (hotdogs * 2.5) + (chips * 1.5) + (soda * 1.25)
-print(f"Your total is: ${total:.2f}") # :.2f floats to 2 decimal places
+for everyletter in master_string:
+     if everyletter.isalpha():
+          list1.append(everyletter.upper())
+#print(list1) #debug to see it worked
+# list1 now has every letter of the master_string
+
+for everyletter in list1:
+     if everyletter not in dictionary1:
+          a = everyletter
+          b = list1.count(everyletter)
+          dictionary1[a] = b
+
+print(dictionary1)
+"""
+from collections import Counter
+dictionary1 = Counter(master_string)
+print(Counter(master_string))
+"""
+s=input()
+t=s.lower()
+
+for everyletter in range(len(master_string)):
+    b=master_string.count(master_string[everyletter])
+    print("{} -- {}".format(master_string[everyletter],b))
